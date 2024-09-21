8b1d46d0683e773472844a7fc61551cb0049612a
AnushaChattopadhyayHeidelberg <anusha.chatto@stud.uni-heidelberg.de>
2024-09-21 17:59:47 +0200

    old code

---
+++ agent_code/final-project-agent-code-old/dqn.py
@@ -0,0 +1,76 @@
+import math
+import random
+import matplotlib
+import matplotlib.pyplot as plt
+from collections import namedtuple, deque
+from itertools import count
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+from .data import create_input
+import random
+
+n_actions = 6 #move in 4 directions,bomb or stay
+n_observations = 289 # this is the pixels on the board, but I am not sure if this is the right approach
+
+'''
+class DQN(nn.Module):
+
+    def __init__(self, n_observations, n_actions):
+        super(DQN, self).__init__()
+        self.layer1 = nn.Linear(n_observations, 128)
+        self.layer2 = nn.Linear(128, 128)
+        self.layer3 = nn.Linear(128, n_actions)
+
+    # Called with either one element to determine next action, or a batch
+    # during optimization. Returns tensor([[left0exp,right0exp]...]).
+    def forward(self, x):
+        x = F.relu(self.layer1(x))
+        x = F.relu(self.layer2(x))
+        return self.layer3(x)
+'''
+
+'''
+class DQN(nn.Module):
+    def __init__(self, n_actions=6):
+        super(DQN, self).__init__()
+        self.conv1 = nn.Conv2d(1, 32, kernel_size=8, stride=4)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=4, stride=2)
+        self.conv3 = nn.Conv2d(64, 64, kernel_size=3, stride=1)
+        self.adaptive_pool = nn.AdaptiveAvgPool2d((11, 11))
+        self.fc1 = nn.Linear(64 * 11 * 11, 512)  
+        self.fc2 = nn.Linear(512, n_actions)
+
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        x = F.relu(self.conv2(x))
+        x = F.relu(self.conv3(x))
+        x = x.view(x.size(0), -1)  
+        x = F.relu(self.fc1(x))
+        return self.fc2(x)
+'''
+
+class DQN(nn.Module):
+    def __init__(self, n_actions=6):
+        super(DQN, self).__init__()
+        self.conv1 = nn.Conv2d(1, 32, kernel_size=4, stride=2, padding=1)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=4, stride=2, padding=1)  
+        self.conv3 = nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1)
+        self.conv4 = nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1)
+        self.conv5 = nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1)
+        self.adaptive_pool = nn.AdaptiveAvgPool2d((11, 11))
+        self.fc1 = nn.Linear(64 * 11 * 11, 512)
+        self.fc2 = nn.Linear(512, n_actions)
+
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        x = F.relu(self.conv2(x))
+        x = F.relu(self.conv3(x))
+        x = F.relu(self.conv4(x))
+        x = F.relu(self.conv5(x))
+        x = self.adaptive_pool(x)  # Apply adaptive pooling
+        x = x.view(x.size(0), -1)
+        x = F.relu(self.fc1(x))
+        return self.fc2(x)
